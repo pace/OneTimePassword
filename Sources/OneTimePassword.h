73a05d670aae62db75afc70d05bf0e61edc452af
Patrick Niepel <patrick@pace.car>
2021-03-26 13:14:54 +0100

    Add bridging header

---
+++ Sources/OneTimePassword.h
@@ -0,0 +1,15 @@
+//
+//  OneTimePassword.h
+//  OneTimePassword
+//
+//  Created by Patrick Niepel on 26.03.21.
+//  Copyright © 2021 Matt Rubin. All rights reserved.
+//
+
+#ifndef OneTimePassword_h
+#define OneTimePassword_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+
+#endif /* OneTimePassword_h */
